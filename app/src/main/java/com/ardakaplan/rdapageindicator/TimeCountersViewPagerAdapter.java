f6accf834db950fa76e965d9489d14b6b32bf88c
Arda Kaplan <arda.kaplan@matriksdata.com>
2018-09-06 14:20:13 +0300

    test

---
+++ app/src/main/java/com/ardakaplan/rdapageindicator/TimeCountersViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.ardroid.ardafatma.ui.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ardakaplan.rdalibrary.ui.adapters.GeneralPagerAdapter;
+import com.ardakaplan.rdapageindicator.R;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by ardakaplan on 18/07/16.
+ * <p/>
+ * www.ardakaplan.com
+ * <p/>
+ * arda.kaplan09@gmail.com
+ */
+public class TimeCountersViewPagerAdapter extends GeneralPagerAdapter<String> {
+
+    public TimeCountersViewPagerAdapter(Activity activity, ArrayList<String> arrayList) {
+        super(activity, arrayList, R.layout.item_viewpager);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View layoutView = (View) super.instantiateItem(container, position);
+
+        String importantDate = getItem(position);
+
+        TextView backgroundImageView = layoutView.findViewById(R.id.item_viewpager_textview);
+
+        backgroundImageView.setText("" + position);
+
+        return layoutView;
+    }
+
+
+}
